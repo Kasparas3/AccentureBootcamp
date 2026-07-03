8d9f52497d086d9bb9b266b4b68f477b7def65d6
Kasparas3 <kasparasmurmoka@gmail.com>
2026-07-03 11:33:02 +0300

    fix: Remove left over comments

--- DBDemo/src/main/resources/insert.sql
+++ DBDemo/src/main/resources/insert.sql
@@ -4,21 +4,12 @@ PRAGMA foreign_keys = ON;
 DELETE FROM students;
 DELETE FROM courses;
 
--- TODO 1:
-
     INSERT INTO courses (course_id, course_name, credits)
     VALUES
         (1, 'Software engineering', 30),
         (2, 'Multimedia', 35),
         (3, 'UX/UI', 20);
 
--- Insert 3 courses.
--- Remember:
--- course_id is a number.
--- course_name must be present.
--- credits must be greater than 0.
-
--- TODO 2:
 
     INSERT INTO students (id, name, email, age, course_id)
     VALUES
@@ -28,11 +19,3 @@ DELETE FROM courses;
         (4, 'Kevin', 'kevin@gmail.com', 19, 1),
         (5, 'Alex', 'alex@gmail.com', 20, 3);
 
-
--- Insert 5 students.
--- Remember:
--- id is a number.
--- name must be present.
--- email must be unique.
--- age must be 18 or older.
--- course_id must exist in the courses table.
