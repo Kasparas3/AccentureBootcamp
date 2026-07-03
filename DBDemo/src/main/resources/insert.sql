4117fa1c764c42c9f605193d89f79c58ca370e60
Kasparas3 <kasparas.murmokas@stud.vilniustech.lt>
2026-07-03 11:35:26 +0300

    Feat/write sql queries (#14)
    * feature: Create database tables
    
    * feature: Insert data into the databases
    
    * fix: Remove left over comments
    
    * feature: Write the query commands

--- DBDemo/src/main/resources/insert.sql
+++ DBDemo/src/main/resources/insert.sql
@@ -4,18 +4,18 @@ PRAGMA foreign_keys = ON;
 DELETE FROM students;
 DELETE FROM courses;
 
--- TODO 1:
--- Insert 3 courses.
--- Remember:
--- course_id is a number.
--- course_name must be present.
--- credits must be greater than 0.
+    INSERT INTO courses (course_id, course_name, credits)
+    VALUES
+        (1, 'Software engineering', 30),
+        (2, 'Multimedia', 35),
+        (3, 'UX/UI', 20);
 
--- TODO 2:
--- Insert 5 students.
--- Remember:
--- id is a number.
--- name must be present.
--- email must be unique.
--- age must be 18 or older.
--- course_id must exist in the courses table.
+
+    INSERT INTO students (id, name, email, age, course_id)
+    VALUES
+        (1, 'Bob', 'bob@gmail.com', 23, 1),
+        (2, 'John', 'john@gmail.com', 18, 2),
+        (3, 'Simon', 'simon@gmail.com', 20, 2),
+        (4, 'Kevin', 'kevin@gmail.com', 19, 1),
+        (5, 'Alex', 'alex@gmail.com', 20, 3);
+
